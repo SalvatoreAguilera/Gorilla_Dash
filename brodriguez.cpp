d38f706590ac99ef251fc2effb58bff7b4fff5c2
Bryan Rodriguez <brodriguez@odin.cs.csubak.edu>
2024-11-08 08:17:58 -0800

    Update brodriguez.cpp

--- brodriguez.cpp
+++ brodriguez.cpp
@@ -1,407 +1,340 @@
+// Bryan Rodriguez
 //
-//modified by: Bryan Rodriguez
-//
-//
-//original author: Gordon Griesel
-//date:            Fall 2024
-//purpose:         OpenGL sample program
-//
-//This program needs some refactoring.
-//We will do this in class together.
-//
-//
+#include "platforms.h"
 #include <iostream>
-using namespace std;
-#include <stdio.h>
 #include <unistd.h>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <cmath>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <GL/glx.h>
-#include <GL/freeglut.h>
-#include <GL/glut.h>
-const int MAX_PARTICLES = 1000;
+#include <cstring>
 
-class Global {
+using namespace std;
+//extern class Image;
+//const int MAX_PARTICLES = 1000;
+/*
+class Image {
 public:
-	int xres, yres;
-	Global();
+    int width, height;
+    unsigned char *data;
+    ~Image() { delete [] data; }
+    Image(const char *fname) {
+        if (fname[0] == '\0')
+            return;
+        char name[40];
+        strcpy(name, fname);
+        int slen = strlen(name);
+        name[slen-4] = '\0';
+        char ppmname[80];
+        sprintf(ppmname, "%s.ppm", name);
+        char ts[100];
+        sprintf(ts, "convert %s %s", fname, ppmname);
+        system(ts);
+        FILE *fpi = fopen(ppmname, "r");
+        if (fpi) {
+            char line[200];
+            fgets(line, 200, fpi);
+            fgets(line, 200, fpi);
+            while (line[0] == '#' || strlen(line) < 2)
+                fgets(line, 200, fpi);
+            sscanf(line, "%i %i", &width, &height);
+            fgets(line, 200, fpi);
+            int n = width * height * 3;
+            data = new unsigned char[n];
+            for (int i = 0; i < n; i++)
+                data[i] = fgetc(fpi);
+            fclose(fpi);
+
+            printf("Image loaded with width: %d, height: %d\n", width, height);
+
+        } else {
+            cout << "ERROR opening image: " << ppmname << endl;
+            exit(0);
+        }
+        unlink(ppmname);
+    }
+};
+*/
+// Texture class to hold texture details
+/*class Platform_Texture {
+public:
+    Platform_Texture* platformTexture;
+    Image *image;
+    GLuint textureID;
+    float xc[2] = {0.0f, 1.0f};  // Horizontal coordinate range
+    float yc[2] = {0.0f, 1.0f};  // Vertical coordinate range
+
+    Platform_Texture(const char *imagePath) {
+        platformTexture = new Platform_Texture("grass_block.jpg");
+        image = new Image(imagePath);
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0,
+                     GL_RGB, GL_UNSIGNED_BYTE, image->data);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+    ~Platform_Texture() { 
+        delete platformTexture;
+        delete image; 
+    }
+};*/
+
+extern class Global {
+public:
+    int xres, yres;
 } g;
 
 class Box {
 public:
-    float pos[10];
-    float last_pos[2];
+    float pos[6]; // Adjusted to hold positions for multiple platforms
     float vel[2];
     int w, h;
     Box() {
-        w = 150;
-        h = 25;
-        //box 1
-        pos[0] = g.xres / 2.15;
-        pos[1] = g.yres / 2;
+        w = 175;
+        h = 20;
+
+        // Set positions for different platforms
+        pos[0] = g.xres / 0.78; // high platform
+        pos[1] = g.yres / 1.6;
         
-        /*//box 2
-        pos[2] = g.xres / 1.6;
-        pos[3] = g.yres / 2.5;*/
+        pos[2] = g.xres / 1.3; // low platform
+        pos[3] = g.yres / 3;
+
+        pos[4] = g.xres / 2.15; // ground platform
+        pos[5] = g.yres / 30;
         
-        //box 3
-        pos[4] = g.xres / 1.3;
-        pos[5] = g.yres / 3.35;
-        
-        /*//box 4
-        pos[6] = pos[0] - w * 2 + (w / 1.5);
-        pos[7] = pos[1] + h * 2.5;*/
-        
-        /*//box 5
-        pos[8] = pos[6] - w * 2 + (w / 1.25);
-        pos[9] = pos[7] + h * 2.5;*/
-        
-        vel[0] = 0.7f; 
+        vel[0] = -1.0f; // velocity for moving platforms
         vel[1] = 0.0f;
     }
 } box;
-//Box particle[MAX_PARTICLES];
-int n = 0;
 
+/*
 class X11_wrapper {
 private:
-	Display *dpy;
-	Window win;
-	GLXContext glc;
+    Display *dpy;
+    Window win;
+    GLXContext glc;
 public:
-	~X11_wrapper();
-	X11_wrapper();
-	void set_title();
-	bool getXPending();
-	XEvent getXNextEvent();
-	void swapBuffers();
-	void reshape_window(int width, int height);
-	void check_resize(XEvent *e);
-	void check_mouse(XEvent *e);
-	int check_keys(XEvent *e);
+    ~X11_wrapper();
+    X11_wrapper();
+    void set_title();
+    bool getXPending();
+    XEvent getXNextEvent();
+    void swapBuffers();
+    void reshape_window(int width, int height);
+    void check_resize(XEvent *e);
+    int check_keys(XEvent *e);
 } x11;
-
-//Function prototypes
-void init_opengl(void);
-void physics(void);
-void render(void);
-
-
+*/
+// Function prototypes
+//void init_opengl(void);
+//void render(void);
+/*
 int main()
 {
-    int argc = 1;
-    const char *argv[1] = { "app" };
-    glutInit(&argc, (char**)argv);
-	init_opengl();
-	int done = 0;
-	//main game loop
-	while (!done) {
-		//look for external events such as keyboard, mouse.
-		while (x11.getXPending()) {
-			XEvent e = x11.getXNextEvent();
-			x11.check_resize(&e);
-			//x11.check_mouse(&e);
-			done = x11.check_keys(&e);
-		}
-		//physics();
-		render();
-		x11.swapBuffers();
-		usleep(200);
-	}
-	return 0;
-}
+    init_opengl();
 
+    int done = 0;
+    // Main game loop
+    while (!done) {
+        // Look for external events such as keyboard
+        while (x11.getXPending()) {
+            XEvent e = x11.getXNextEvent();
+            x11.check_resize(&e);
+            done = x11.check_keys(&e);
+        }
+        render();
+        x11.swapBuffers();
+        usleep(20000); // Sleep for a while
+    }
+    return 0;
+}
+*/
+/*
 Global::Global()
 {
-	xres = 800;
-	yres = 600;
+    xres = 800;
+    yres = 600;
 }
 
 X11_wrapper::~X11_wrapper()
 {
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
+    XDestroyWindow(dpy, win);
+    XCloseDisplay(dpy);
 }
-
-X11_wrapper::X11_wrapper()
+*/
+/*X11_wrapper::X11_wrapper()
 {
-	GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-	int w = g.xres, h = g.yres;
-	dpy = XOpenDisplay(NULL);
-	if (dpy == NULL) {
-		cout << "\n\tcannot connect to X server\n" << endl;
-		exit(EXIT_FAILURE);
-	}
-	Window root = DefaultRootWindow(dpy);
-	XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
-	if (vi == NULL) {
-		cout << "\n\tno appropriate visual found\n" << endl;
-		exit(EXIT_FAILURE);
-	} 
-	Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
-	XSetWindowAttributes swa;
-	swa.colormap = cmap;
-	swa.event_mask =
-		ExposureMask | KeyPressMask | KeyReleaseMask |
-		ButtonPress | ButtonReleaseMask |
-		PointerMotionMask |
-		StructureNotifyMask | SubstructureNotifyMask;
-	win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-		InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
-	set_title();
-	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
-	glXMakeCurrent(dpy, win, glc);
+    GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+    int w = g.xres, h = g.yres;
+    dpy = XOpenDisplay(NULL);
+    if (dpy == NULL) {
+        cout << "\n\tcannot connect to X server\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    Window root = DefaultRootWindow(dpy);
+    XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+    if (vi == NULL) {
+        cout << "\n\tno appropriate visual found\n" << endl;
+        exit(EXIT_FAILURE);
+    } 
+    Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+    XSetWindowAttributes swa;
+    swa.colormap = cmap;
+    swa.event_mask =
+        ExposureMask | KeyPressMask | KeyReleaseMask |
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
+    win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
+                        InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+    set_title();
+    glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+    glXMakeCurrent(dpy, win, glc);
 }
 
 void X11_wrapper::set_title()
 {
-	//Set the window title bar.
-	XMapWindow(dpy, win);
-	XStoreName(dpy, win, "3350 Lab-6");
+    // Set the window title bar.
+    XMapWindow(dpy, win);
+    XStoreName(dpy, win, "3350 Lab-6");
 }
 
 bool X11_wrapper::getXPending()
 {
-	//See if there are pending events.
-	return XPending(dpy);
+    // See if there are pending events.
+    return XPending(dpy);
 }
 
 XEvent X11_wrapper::getXNextEvent()
 {
-	//Get a pending event.
-	XEvent e;
-	XNextEvent(dpy, &e);
-	return e;
+    // Get a pending event.
+    XEvent e;
+    XNextEvent(dpy, &e);
+    return e;
 }
 
 void X11_wrapper::swapBuffers()
 {
-	glXSwapBuffers(dpy, win);
+    glXSwapBuffers(dpy, win);
 }
 
 void X11_wrapper::reshape_window(int width, int height)
 {
-	//Window has been resized.
-	g.xres = width;
-	g.yres = height;
-	//
-	glViewport(0, 0, (GLint)width, (GLint)height);
-	glMatrixMode(GL_PROJECTION); glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
+    // Window has been resized.
+    g.xres = width;
+    g.yres = height;
+    glViewport(0, 0, (GLint)width, (GLint)height);
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
 }
 
 void X11_wrapper::check_resize(XEvent *e)
 {
-	//The ConfigureNotify is sent by the
-	//server if the window is resized.
-	if (e->type != ConfigureNotify)
-		return;
-	XConfigureEvent xce = e->xconfigure;
-	if (xce.width != g.xres || xce.height != g.yres) {
-		//Window size did change.
-		reshape_window(xce.width, xce.height);
-	}
+    // The ConfigureNotify is sent by the server if the window is resized.
+    if (e->type != ConfigureNotify)
+        return;
+    XConfigureEvent xce = e->xconfigure;
+    if (xce.width != g.xres || xce.height != g.yres) {
+        // Window size did change.
+        reshape_window(xce.width, xce.height);
+    }
 }
-//-----------------------------------------------------------------------------
-
-#define rnd() (float)rand() / (float)RAND_MAX
-/*void X11_wrapper::check_mouse(XEvent *e)
-{
-	static int savex = 0;
-	static int savey = 0;
-
-	//Weed out non-mouse events
-	if (e->type != ButtonRelease &&
-		e->type != ButtonPress &&
-		e->type != MotionNotify) {
-		//This is not a mouse event that we care about.
-		return;
-	}
-	//
-	if (e->type == ButtonRelease) {
-		return;
-	}
-	if (e->type == ButtonPress) {
-		if (e->xbutton.button==1) {
-			//Left button was pressed.
-			int y = g.yres - e->xbutton.y;
-			//make particle
-            if (n < MAX_PARTICLES) {
-                particle[n].pos[0] = e->xbutton.x;
-                particle[n].pos[1] = y;
-                particle[n].w = 4;
-                particle[n].h = 4;
-                particle[n].vel[0] = 0.0f;
-                particle[n].vel[1] = 0.0f;
-                ++n;
-            }
-            return;
-		}
-		if (e->xbutton.button==3) {
-			//Right button was pressed.
-			return;
-		}
-	}
-	if (e->type == MotionNotify) {
-		//The mouse moved!
-		if (savex != e->xbutton.x || savey != e->xbutton.y) {
-			savex = e->xbutton.x;
-			savey = e->xbutton.y;
-			//Code placed here will execute whenever the mouse moves. 
-			int y = g.yres - e->xbutton.y;
-            for (int i=0; i<2; i++)
-            if (n < MAX_PARTICLES) {
-                particle[n].pos[0] = e->xbutton.x;
-                particle[n].pos[1] = y;
-                particle[n].w = 4;
-                particle[n].h = 4;
-                particle[n].vel[0] = rnd() * 1.0 - 0.5;
-                particle[n].vel[1] = rnd() * 1.0 - 0.5;
-                ++n;
-            }
-
-		}
-	}
-}*/
 
 int X11_wrapper::check_keys(XEvent *e)
 {
-	if (e->type != KeyPress && e->type != KeyRelease)
-		return 0;
-	int key = XLookupKeysym(&e->xkey, 0);
-	if (e->type == KeyPress) {
-		switch (key) {
-			case XK_a:
-				//the 'a' key was pressed
-				break;
-			case XK_Escape:
-				//Escape key was pressed
-				return 1;
-		}
-	}
-	return 0;
-}
-
-void init_opengl(void)
-{
-	//OpenGL initialization
-	glViewport(0, 0, g.xres, g.yres);
-	//Initialize matrices
-	glMatrixMode(GL_PROJECTION); glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	//Set 2D mode (no perspective)
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
-	//Set the screen background color
-	glClearColor(0.1, 0.1, 0.1, 1.0);
-}
-/*
-const float GRAVITY = -0.1;
-void physics()
-{
-    for (int i = 0; i < n; i++) {
-        // Apply gravity
-        particle[i].vel[1] += GRAVITY;
-        particle[i].last_pos[0] = particle[i].pos[0];
-        particle[i].last_pos[1] = particle[i].pos[1];
-        particle[i].pos[0] += particle[i].vel[0];
-        particle[i].pos[1] += particle[i].vel[1];
-    }
-    
-    // Collision detection for all boxes
-    for (int i = 0; i < n; i++) {
-        // Iterate over the boxes (each box has an x and y position)
-        for (int j = 0; j < 9; j += 2) {
-            // Check if particle is within the bounds of the current box
-            if (particle[i].pos[1] < box.pos[j+1] + box.h &&
-                particle[i].pos[1] > box.pos[j+1] - box.h &&
-                particle[i].pos[0] > box.pos[j] - box.w &&
-                particle[i].pos[0] < box.pos[j] + box.w) {
-                // In a collision state with box j/2 (box #1, #2, etc.)
-                particle[i].pos[1] = particle[i].last_pos[1];
-                particle[i].vel[1] = -particle[i].vel[1] * 0.5;
-                particle[i].vel[0] += rnd() * 0.04;
-            }
-        }
-        // Remove particle if it goes off-screen
-        if (particle[i].pos[1] < 0) {
-            particle[i] = particle[--n];
+    if (e->type != KeyPress && e->type != KeyRelease)
+        return 0;
+    int key = XLookupKeysym(&e->xkey, 0);
+    if (e->type == KeyPress) {
+        switch (key) {
+            case XK_a:
+                break;
+            case XK_Escape:
+                // Escape key was pressed
+                return 1;
         }
     }
+    return 0;
 }
 */
-/*void renderBitmapString(float x, float y, void *font, const char *string) {
-    glRasterPos2f(x, y);
-    for (const char *c = string; *c != '\0'; c++) {
-        glutBitmapCharacter(font, *c);
-    }
-}
-
-void renderText(const char* message) {
-    glColor3f(1.0f, 1.0f, 1.0f);
-
-    float textWidth = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, 
-                                       (const unsigned char*)message);
-    float textX = -(textWidth / 2.0f); // Center horizontally
-    float textY = -4.0f; // Center vertically
-
-    glRasterPos2f(textX, textY);
-
-    renderBitmapString(textX, textY, GLUT_BITMAP_HELVETICA_18, message);
-}*/
-
-void render()
+//Platform_Texture *platformTexture;
+/*
+void init_opengl(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	//draw 5 boxes
-    for (int i = 0; i < 9; i++) {
+    // OpenGL initialization
+    glViewport(0, 0, g.xres, g.yres);
+    // Initialize matrices
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+    // Set 2D mode (no perspective)
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
+    // Set the screen background color
+    glClearColor(0.1, 0.1, 0.1, 1.0);
+    
+    // Enable textures
+    glEnable(GL_TEXTURE_2D);
+    // Load platform texture
+    platformTexture = new Texture("grass_block.jpg");
+}
+*/
+//Platform_Texture PlatformTexture;
+void render_platforms()
+{
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    //glEnable(GL_TEXTURE_2D);
+    //glBindTexture(GL_TEXTURE_2D, PlatformTexture.textureID);
+
+    // draw 2 platforms
+    for (int i = 0; i < 3; i++) {
+        // move platforms to the left
+        box.pos[i] += box.vel[0];
+
+        // reappear platforms after moving offscreen
+        if (box.pos[i] < -box.w)
+            box.pos[i] = g.xres + box.w;
+        
         glPushMatrix();
-	    glColor3ub(50, 205, 50);
+	    glColor3ub(0, 100, 0);
 	    glTranslatef(box.pos[i]+1, box.pos[i+1], 0.0f);
 	    glBegin(GL_QUADS);
-		    glVertex2f(-box.w, -box.h);
-		    glVertex2f(-box.w,  box.h);
-		    glVertex2f( box.w,  box.h);
-		    glVertex2f( box.w, -box.h);
+		    //glTexCoord2f(PlatformTexture.xc[0], PlatformTexture.yc[0]); 
+            glVertex2f(-box.w, -box.h);
+		    //glTexCoord2f(PlatformTexture.xc[0], PlatformTexture.yc[1]); 
+            glVertex2f(-box.w,  box.h);
+		    //glTexCoord2f(PlatformTexture.xc[1], PlatformTexture.yc[1]); 
+            glVertex2f( box.w,  box.h);
+		    //glTexCoord2f(PlatformTexture.xc[1], PlatformTexture.yc[0]); 
+            glVertex2f( box.w, -box.h);
 	    glEnd();
 	    
-       /* //Text in boxes
-        switch (i / 2) {
-            case 0:
-                renderText("Coding");
-                break;
-            case 1:
-                renderText("Testing");
-                break;
-            case 2:
-                renderText("Maintenance");
-                break;
-            case 3:
-                renderText("Design");
-                break;
-            case 4:
-                renderText("Requirements");
-                break;
-        }*/
-
         glPopMatrix();
         i++;
     }
-    /*//draw a particle
-	for (int i=0; i<n; i++) {
-        glPushMatrix();
-	    glColor3ub(100, 120, 220);
-	    glTranslatef(particle[i].pos[0], particle[i].pos[1], 0.0f);
-	    glBegin(GL_QUADS);
-		    glVertex2f(-particle[i].w, -particle[i].h);
-		    glVertex2f(-particle[i].w,  particle[i].h);
-		    glVertex2f( particle[i].w,  particle[i].h);
-		    glVertex2f( particle[i].w, -particle[i].h);
-	    glEnd();
-	    glPopMatrix();
-    }*/
+
+    //glBindTexture(GL_TEXTURE_2D, 0);
+    //glDisable(GL_TEXTURE_2D);
+
+    // Ground platform separately--don't want it moving
+    glPushMatrix();
+    glColor3ub(0, 100, 0);
+    glTranslatef(box.pos[4], box.pos[5], 0.0f);
+    glBegin(GL_QUADS);
+        //glTexCoord2f(PlatformTexture.xc[0], PlatformTexture.yc[0]); 
+        glVertex2f(-g.xres, -box.h);
+        //glTexCoord2f(PlatformTexture.xc[0], PlatformTexture.yc[1]); 
+        glVertex2f(-g.xres,  box.h);
+        //glTexCoord2f(PlatformTexture.xc[1], PlatformTexture.yc[1]); 
+        glVertex2f( g.xres,  box.h);
+        //glTexCoord2f(PlatformTexture.xc[1], PlatformTexture.yc[0]); 
+        glVertex2f( g.xres, -box.h);
+    glEnd();
+    glPopMatrix();
+    // unbind texture
+    //glBindTexture(GL_TEXTURE_2D, 0);
+    //glDisable(GL_TEXTURE_2D);
 }
+
