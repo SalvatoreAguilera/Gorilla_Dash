b1e7243d0ab3ccd10c67ea0e0cfd191fd281823d
Bryan Rodriguez <brodriguez@odin.cs.csubak.edu>
2024-12-04 02:36:45 -0800

    Update rmoreno2.cpp

--- rmoreno2.cpp
+++ rmoreno2.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <cstdio>
 
+extern bool end_screen;
 extern class Global {
     public:
         int xres, yres;
@@ -260,8 +261,9 @@ void init_hazards()
 
 void take_damage(int damage) {
     current_health -= damage;
-    if (current_health < 0)
-        current_health = 0;
+    if (current_health == 0)
+        end_screen = true;
+        //current_health = 0;
 }
 
 void heal(int amount) {
