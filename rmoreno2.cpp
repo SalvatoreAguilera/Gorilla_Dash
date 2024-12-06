848e844808a37a71b9fda7e4a7e232a242ddbd17
bryanrodrig <brodriguez2@csub.edu>
2024-12-05 16:32:00 -0800

    Update rmoreno2.cpp

--- rmoreno2.cpp
+++ rmoreno2.cpp
@@ -262,7 +262,7 @@ void init_hazards()
 
 void take_damage(int damage) {
     current_health -= damage;
-    if (current_health == 0)
+    if (current_health == 0 || current_health < 0)
         end_screen = true;
 }
 
